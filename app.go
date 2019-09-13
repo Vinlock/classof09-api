817ae1c36b7e8f88fbdefbbf8f23a2afe51b2efb
Dak Washbrook <vinlockz@gmail.com>
2019-09-12 19:45:42 -0700

    Added bugsnag

--- app.go
+++ app.go
@@ -5,9 +5,12 @@ import (
 	"ecr-reunion/controllers"
 	"ecr-reunion/db"
 	"fmt"
+	"github.com/bugsnag/bugsnag-go"
+	bugsnagGin "github.com/bugsnag/bugsnag-go/gin"
 	"github.com/fvbock/endless"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"log"
 	"os"
 	"time"
 )
@@ -17,7 +20,25 @@ func main() {
 	if ginIsReleaseMode {
 		gin.SetMode(gin.ReleaseMode)
 	}
+
+	bugsnag.Configure(bugsnag.Configuration{
+		APIKey: os.Getenv("APP_BUGSNAG_API_KEY"),
+		// The import paths for the Go packages containing your source files
+		ProjectPackages: []string{"main", "ecr-reunion"},
+		AppVersion:      os.Getenv("APP_COMMIT_SHA"),
+	})
+
 	r := gin.Default()
+
+	r.Use(bugsnagGin.AutoNotify())
+
+	r.GET("/test", func(c *gin.Context) {
+		err := bugsnag.Notify(fmt.Errorf("Test error"))
+		if err != nil {
+			log.Panic("BUGSNAG_ERROR")
+		}
+		c.JSON(200, gin.H{})
+	})
 
 	// Cors
 	r.Use(cors.New(cors.Config{
